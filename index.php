cc266c6d19bab87b1238df4007aac80e516e8659
Jonathan Vingiano <jvingiano@gmail.com>
2010-07-01 14:51:31 -0400

    cleaning up

--- index.php
+++ index.php
@@ -7,27 +7,26 @@ $published_posts = $count_posts->publish;
 
 <a href="mailto:<?php bloginfo('admin_email'); ?>"><?php print print_logo(); ?></a>
 
-?>
 
 <div id="main" style="width:<?php echo $published_posts * 620; ?>px;">
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		<script type='text/javascript'> 
-					$(function() {
-    			$('#s<?php the_ID(); ?>')
-					.before	('<div id="n<?php the_ID(); ?>" class="nav">')
-						.cycle({
-						fx: 'fade',
-						speed: 'fast',
-						timeout: 0,
-						next: '#s<?php the_ID(); ?>',
-						pager:  '#n<?php the_ID(); ?>',
-				});
+		$(document).ready(function() {
+			$('#s<?php the_ID(); ?>')
+				.before	('<div id="n<?php the_ID(); ?>" class="nav">')
+				.cycle({
+					fx: 'fade',
+					speed: 'fast',
+					timeout: 0,
+					next: '#s<?php the_ID(); ?>',
+					pager:  '#n<?php the_ID(); ?>',
 			});
+		});
 		</script>
-		<div id="post-<?php the_ID(); ?>" class="sup">
-		<div id="s<?php the_ID();?>" class="images">
+		<div id="post-<?php the_ID(); ?>" class="thepost">
+			<div id="s<?php the_ID();?>" class="images">
 		<?php the_content(); ?>
-		</div>
+			</div>
 		</div>
 
 <?php endwhile; ?>
