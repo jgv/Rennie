bddd1b3b435fb3989d8f603fb8f9a7f3a9d720b0
Jonathan Vingiano <jvingiano@gmail.com>
2010-07-01 00:49:33 -0400

    updated typography, js cycle

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
-<script type="text/javascript" src="<?php bloginfo('template_directory') ?>/js/jquery.cycle.lite.1.0.min.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory') ?>/js/jquery.cycle.all.latest.js"></script>
 <script type="text/javascript" charset="utf-8">
 	$(function(){
 		$("#main").wrapInner("<table><tr>");
--- index.php
+++ index.php
@@ -6,22 +6,23 @@ $published_posts = $count_posts->publish;
 ?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
-		
 		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<script type='text/javascript'> 
+		<script type='text/javascript'> 
 			$(document).ready(function() {
-    			$('.s<?php the_ID(); ?>').cycle({
+    			$('#s<?php the_ID(); ?>')
+    			.after('<div id="nav<?php the_ID(); ?>">')
+    			.cycle({
 				fx: 'fade',
-				next: '.s<?php the_ID(); ?>', 
-    			timeout: 0 
+				next: '#s<?php the_ID(); ?>', 
+    			timeout: 0, 
+    			pager:  '#nav<?php the_ID(); ?>'
 			});
 		});
 		</script>
-		
-		<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-		<div class='s<?php the_ID();?>'>
+		<div id="s<?php the_ID();?>">
 		<?php the_content(); ?>
 		</div>
+		<?php the_excerpt(); ?>
 		</div>
 		<?php endwhile;?>
 
