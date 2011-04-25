9e36d544e90ba7ad470ea92ae31d7abd5b0d877d
jgv <jvingiano@gmail.com>
2011-04-25 17:12:20 -0400

    jquery 1.5.2

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
 <script src="<?php bloginfo('template_directory') ?>/js/jquery.plugins.js"></script>
 <script type="text/javascript">
 $(document).ready(function(){
